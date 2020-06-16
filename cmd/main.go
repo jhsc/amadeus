6f0d4a7bc7b962770e5142f812f091586366e02a
jhsc <hi@jhsc.io>
2020-06-16 16:01:36 -0700

    added service commands

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,55 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 )
 
 var (
-	logger = log.New(os.Stdout, "", log.LstdFlags|log.LUTC)
+	logger       = log.New(os.Stdout, "", log.LstdFlags|log.LUTC)
+	useEnvConfig = flag.Bool("e", false, "use environment variables as config")
 )
 
 func main() {
-	logger.Println("hello :)")
+	flag.Usage = help
+	flag.Parse()
+
+	commands := map[string]func(){
+		"start":   startServer,
+		"init":    initConfig,
+		"gen-key": genKey,
+		"help":    help,
+	}
+
+	if cmdFunc, ok := commands[flag.Arg(0)]; ok {
+		cmdFunc()
+	} else {
+		help()
+		//  Misuse of shell builtins
+		os.Exit(1)
+	}
+}
+
+func startServer() {
+	logger.Println("start server srv")
+}
+
+func initConfig() {
+	logger.Println("initiliaze config srv")
+}
+
+func genKey() {
+	logger.Println("gen keys srv")
+}
+
+func help() {
+	fmt.Fprintln(os.Stderr, `Usage:
+	amadeus start                      - start the server
+	amadeus init                       - create an initial configuration file
+	amadeus gen-key                    - generate a random 32-byte hex-encoded key
+	amadeus help                       - show this message
+Use -e flag to read configuration from environment variables instead of a file. E.g.:
+	amadeus -e start`)
 }
