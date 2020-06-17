e8455ca509e9ce64a8367a37b4a8acf667360d12
jhsc <hi@jhsc.io>
2020-06-16 17:16:54 -0700

    added init config command wich genrates config file

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+
+	"gitlab.com/jhsc/amadeus/config"
 )
 
 var (
@@ -37,7 +40,21 @@ func startServer() {
 }
 
 func initConfig() {
-	logger.Println("initiliaze config srv")
+	if _, err := os.Stat(config.ConfigFile); !os.IsNotExist(err) {
+		logger.Fatalf("configuration file already exists: %s", config.ConfigFile)
+	}
+
+	logger.Printf("creating initial configuration: %s", config.ConfigFile)
+
+	cfg, err := config.Init()
+	if err != nil {
+		logger.Fatalf("failed to generate initial configuration: %s", err)
+	}
+
+	err = ioutil.WriteFile(config.ConfigFile, []byte(cfg), 0666)
+	if err != nil {
+		logger.Fatalf("failed to write configuration file: %s", err)
+	}
 }
 
 func genKey() {
