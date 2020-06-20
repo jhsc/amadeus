0391484ff085f830ca71d50529d2ba47c23aa5ad
jhsc <jsantacruz13@gmail.com>
2020-06-19 23:00:05 -0700

    bind to 0.0.0.0

--- config/config.go
+++ config/config.go
@@ -96,7 +96,7 @@ func GenKeyHex(byteLen int) string {
 }
 
 var tpl = template.Must(template.New("initial-config").Parse(strings.TrimSpace(`
-address 	= "127.0.0.1:8080"
+address 	= "0.0.0.0:8080"
 base_url 	= "https://amadeus.com"
 title    	= "amadeus"
 token 	 	= "{{.token_secret}}"
