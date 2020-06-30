4302c3c686189b28d929e71434649208307efc15
jhsc <hi@jhsc.io>
2020-06-30 12:00:48 -0700

    remove some lines from health route

--- api/healthz.go
+++ api/healthz.go
@@ -6,13 +6,6 @@ import (
 
 // HandleHealthz renders 200 status if server is running
 func (h *Handler) HandleHealthz(w http.ResponseWriter, r *http.Request) {
-	id, err := h.Store.Projects().New("Test Project")
-	if err != nil {
-		h.logError("create new project: %s", err)
-		h.renderError(w, http.StatusInternalServerError, "ServerError", "Server error")
-		return
-	}
-	h.Logger.Printf("Created project with id: %d\n", id)
 	response := struct {
 		Message string `json:"message"`
 	}{
